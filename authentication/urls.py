ffd2ec87eedd2ba606fc0938f727eb71fde27c35
Philipe Vieira <philipe@stsinformatica.com>
2022-12-22 00:49:35 -0300

    creating logout and secure route

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('register', views.register, name='register'),
     path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
+    path('secure', views.secure, name='secure'),
 ]
