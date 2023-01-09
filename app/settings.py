a1a42f23674fc83b57a044ed351f36a6e7af3406
Philipe Vieira Oliveira <philipefilho@hotmail.com>
2023-01-09 11:04:03 -0300

    creating reset password

--- app/settings.py
+++ app/settings.py
@@ -135,3 +135,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_FROM_ADDRESS = env('EMAIL_FROM_ADDRESS')
