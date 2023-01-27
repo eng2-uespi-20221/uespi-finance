faacf24786c7e3c4cbce06fd1ec47033be1cd9b7
Adiel Lima <adiel.lima.589@gmail.com>
2023-01-27 12:24:56 -0300

    funcionando

--- crudTransacao/models.py
+++ crudTransacao/models.py
@@ -3,9 +3,9 @@ import datetime
 
 class Transacao(models.Model):
     id = models.AutoField(primary_key=True)
-    data = models.DateField(default=datetime)
+    data = models.DateField()
     descricao = models.CharField(null=True, max_length=60)
-    tipo_transacao = models.BooleanField(null=True, default=1)
+    tipo_transacao = models.CharField(null=True, max_length=60)
     valor = models.FloatField(null=True, default=0)
 
 
