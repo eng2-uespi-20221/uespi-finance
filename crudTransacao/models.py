1be14973c0beca47767a2669b95962d3d83b3e57
Adiel Lima <adiel.lima.589@gmail.com>
2023-01-27 10:38:40 -0300

    alteracao models

--- crudTransacao/models.py
+++ crudTransacao/models.py
@@ -2,18 +2,11 @@ from django.db import models
 import datetime
 
 class Transacao(models.Model):
-<<<<<<< HEAD
     id = models.AutoField(primary_key=True)
     data = models.DateField(default=datetime)
     descricao = models.CharField(null=True, max_length=60)
     tipo_transacao = models.BooleanField(null=True, default=1)
     valor = models.FloatField(null=True, default=0)
-=======
-    data = models.DateField()
-    descricao = models.CharField(max_length=60)
-    tipo_transacao = models.BooleanField(default=1)
-    valor = models.FloatField()
->>>>>>> 74e420318f0a00d314ddeb56a37c66ae0fb83bfb
 
 
 
