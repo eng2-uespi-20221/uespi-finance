19f603e52b85101ec8562d91dd792acfe9b41cd9
Sergio Avila <uespicc@gmail.com>
2023-01-27 09:29:49 -0300

    app budget - criação e CRUD

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# APP BUDGET
+class Budget(models.Model):
+    id = models.IntegerField(primary_key=True)
+    dataCriacao = models.DateField()
+    orcamento = models.CharField(max_length=60)
--- crudTransacao/models.py
+++ crudTransacao/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
 class Transacao(models.Model):
-    id = models.IntegerField(primary_key=True)
     data = models.DateField()
     descricao = models.CharField(max_length=60)
     tipo_transacao = models.BooleanField(default=1)
     valor = models.FloatField()
 
-
-    
 
 
