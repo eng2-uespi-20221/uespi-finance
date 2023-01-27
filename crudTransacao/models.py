ee9526819fd71400fd410e2f0b693a9ea31859d4
Adiel Lima <adiel.lima.589@gmail.com>
2023-01-27 16:30:11 -0300

    delete e create True

--- crudTransacao/models.py
+++ crudTransacao/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-import datetime
 
 class Transacao(models.Model):
     id = models.AutoField(primary_key=True)
-    data = models.DateField()
+    data = models.DateField(null=True)
     descricao = models.CharField(null=True, max_length=60)
     tipo_transacao = models.CharField(null=True, max_length=60)
     valor = models.FloatField(null=True, default=0)
--- crudTransacao/views.py
+++ crudTransacao/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def vdelete(request, id):
-    transacaos = get_object_or_404(Transacao, id=id)
-    transacaos.delete()
+    transacao = get_object_or_404(Transacao, id=id)
+    transacao.delete()
     return redirect('vcreate')
 
 def vupdate(request):
@@ -14,7 +14,9 @@ def vupdate(request):
 
 
 def vread(request):
+
     transacaos = Transacao.objects.all()
+    #print(request.user.id)
     return render(request, 'readT.html', {
         "transacaos": transacaos
     })
@@ -26,16 +28,17 @@ def abrircreate(request):
 
 def vcreate(request):
     # obtem o dado nome do form e armazena na var vnome
-    vdata = request.POST.get('data')
-    vdescricao = request.POST.get('descricao')
-    vtipo_transacao = request.POST.get('tipo_transacao')
-    vvalor = request.POST.get('valor')
-    # cria um registro no bd no campo nome passando a ver vnome
-    Transacao.objects.create(data=vdata, descricao=vdescricao, tipo_transacao=vtipo_transacao, valor=vvalor)
-    # envia lista atualizada do bd para o index.html
-    transacaos = Transacao.objects.all()
-    # recarrega a página index.html com os dados atualizados
-    return render(request, 'readT.html', {'transacaos': transacaos})
+    if (request.method == 'POST'):
+        vdata = request.POST.get('data')
+        vdescricao = request.POST.get('descricao')
+        vtipo_transacao = request.POST.get('tipo_transacao')
+        vvalor = request.POST.get('valor')
+        # cria um registro no bd no campo nome passando a ver vnome
+        Transacao.objects.create(data=vdata, descricao=vdescricao, tipo_transacao=vtipo_transacao, valor=vvalor)
+        # envia lista atualizada do bd para o index.html
+        transacaos = Transacao.objects.all()
+        # recarrega a página index.html com os dados atualizados
+    return redirect(vread)
 
 
 
