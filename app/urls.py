ddd332975464684d0e39a1af07df2544c35c8069
Philipe Vieira Oliveira <philipefilho@hotmail.com>
2023-01-27 23:51:42 -0300

    redirecionando o raiz para o welcome

--- app/urls.py
+++ app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('welcome')), 
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('template/', TemplateView.as_view(template_name='layouts/base.html')),
