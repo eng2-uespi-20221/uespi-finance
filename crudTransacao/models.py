ea537df3b33ac1452fd3759c9d6dd96d97c0cf31
Sergio Avila <uespicc@gmail.com>
2023-01-28 21:44:57 -0300

    apos correção casas decimais

--- crudTransacao/models.py
+++ crudTransacao/models.py
@@ -4,7 +4,7 @@ class Transacao(models.Model):
     data = models.DateField(auto_now_add=True)
     descricao = models.CharField(null=True, max_length=60)
     tipo_transacao = models.CharField(null=True, max_length=60)
-    valor = models.FloatField(null=True, default=0)
+    valor = models.DecimalField(null=True, default=0,decimal_places=2,max_digits=5)
 
 
 
