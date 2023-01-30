e074836a269809c0eb82ff24d0296f5263731a74
Sergio Avila <uespicc@gmail.com>
2023-01-29 23:03:18 -0300

    Update migration

--- budget/models.py
+++ budget/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 # APP BUDGET
 class Budget(models.Model):    
     orcamento = models.CharField(max_length=60)
-    valor_limite = models.FloatField(default=0)
+    valor_limite = models.DecimalField(default=0,decimal_places=2,max_digits=20)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING,default=1)
