371f3056d4e5c46878afb1fa17ca401254478602
Sergio Avila <uespicc@gmail.com>
2023-02-01 23:50:48 -0300

    add chave estrangeira em models/budget

--- category/models.py
+++ category/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
     type_transaction = models.BooleanField(default=0)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING,default=1)
     
     
