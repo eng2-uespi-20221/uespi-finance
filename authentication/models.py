a951e36e56fb927750813961ef9f95bde4d910b9
Philipe Vieira Oliveira <philipefilho@hotmail.com>
2023-02-02 07:55:16 -0300

    criando a conta do usuário

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    balance = models.DecimalField(default=0,decimal_places=2,max_digits=20)
+    income = models.DecimalField(default=0,decimal_places=2,max_digits=20)
+    expense = models.DecimalField(default=0,decimal_places=2,max_digits=20)
+    
--- crudTransacao/views.py
+++ crudTransacao/views.py
@@ -1,11 +1,21 @@
 from .models import Transacao, Category
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from authentication.models import Account
+from decimal import Decimal
 
 # Create your views here.
 
 def vdelete(request, id):
     transacao = get_object_or_404(Transacao, id=id)
+
+    if transacao.categoria.type_transaction: # expense
+        request.user.account.balance += transacao.valor
+        request.user.account.expense -= transacao.valor
+    else: # income
+        request.user.account.balance -= transacao.valor
+        request.user.account.income -= transacao.valor
+    request.user.account.save()
     transacao.delete()
     return redirect('transacao')
 
@@ -31,6 +41,32 @@ def vupdate(request, id):
         #obtem id do registro
         category  = Category.objects.get_or_create(name=vcategoria)[0]
         transacao = Transacao.objects.get(id=id)
+
+        # Se tiver alteração de valor ou categoria da transação
+        if Decimal(transacao.valor) != Decimal(valor) or transacao.categoria != category: 
+            # Era receita e se tornou despesa
+            if transacao.categoria.type_transaction != category.type_transaction and category.type_transaction: #expense
+                request.user.account.balance -= transacao.valor + valor
+                request.user.account.income -= transacao.valor + valor
+                request.user.account.expense += valor
+            
+            # Era despesa se tornou receita
+            elif transacao.categoria.type_transaction != category.type_transaction and not category.type_transaction: # income
+                request.user.account.balance += transacao.valor + valor
+                request.user.account.income += valor
+                request.user.account.expense -= transacao.valor
+
+            # mantem mesmo tipo
+            else:
+                diferenca = Decimal(transacao.valor) - Decimal(valor)
+
+                if transacao.categoria.type_transaction: # expense
+                    request.user.account.balance += diferenca
+                    request.user.account.expense -= diferenca
+                else: # income
+                    request.user.account.balance -= diferenca
+                    request.user.account.income -= diferenca
+            request.user.account.save()
         
         transacao.descricao = vdescricao
         transacao.tipo_transacao = vtipo_transacao
@@ -43,10 +79,16 @@ def vupdate(request, id):
 
 @login_required(login_url='login')
 def vread(request):
+    try:
+        account = request.user.account
+    except:
+        account = Account.objects.create(user=request.user)
+
     transacaos = Transacao.objects.filter(user_id=request.user.id)
     #print(request.user.id)
     return render(request, 'readT.html', {
-        "transacaos": transacaos
+        "transacaos": transacaos,
+        "account": account
     })
 
 def abrircreate(request):
@@ -63,15 +105,23 @@ def vcreate(request):
         vcategoria = request.POST.get('categoria')
         vvalor = request.POST.get('valor')
         valorformatado = str(vvalor).replace(',','.')
+        valorformatado = Decimal(valorformatado)
 
-        
         # Check if all required fields are filled
         if not vdescricao or not vvalor or not vcategoria:
             return render(request, 'createT.html')
 
-        else:
-            category, created = Category.objects.get_or_create(id=vcategoria)
-            Transacao.objects.create(data=vdata, descricao=vdescricao, categoria=category, valor=valorformatado, user=request.user)
+        category, created = Category.objects.get_or_create(id=vcategoria)
+
+        if category.type_transaction: # expense
+            request.user.account.balance -= valorformatado
+            request.user.account.expense += valorformatado
+        else: # income
+            request.user.account.balance += valorformatado
+            request.user.account.income += valorformatado
+        request.user.account.save()
+
+        Transacao.objects.create(data=vdata, descricao=vdescricao, categoria=category, valor=valorformatado, user=request.user)
 
     return redirect(vread)
 
